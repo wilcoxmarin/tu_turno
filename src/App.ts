af2a31476b14d071b513425c8eec2e43e4b77c49
Wilcox Marin <wilcoxmarin@gmail.com>
2021-04-01 09:37:39 -0500

    ajustes

--- src/App.ts
+++ src/App.ts
@@ -2,9 +2,5 @@
 const enviroment = require('./config/enviroment')
 // importamos express
 import server from "./config/server";
-//
-import routers from "./router/router"
-console.log(routers)
-server.use(routers)
 // iniciamos el servidor
 server.listen(enviroment.PORT, () => console.log(`Running on port ${enviroment.PORT}`))
